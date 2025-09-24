8d835d36d36502e99df7bfc27f40f823e1afd9d7
Dhamu-code <dhamodharan130602@gmail.com>
2025-09-24 16:39:36 +0530

    Feat: JS basics operators

---
+++ JsBasics/Operators.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 20;
+
+// Arithmetic operators
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a++);
+console.log(--b);
+
+// Assignment operators
+
+console.log(a += a);
+console.log(a -= a);
+console.log(b *= b);
+console.log(b /= b);
+
+// Comparison and Logical operator
+
+console.log(a === 10 && b === 20);
+console.log(a != 12 || b !== 22);
