68a400a34edf97ae90bbcc1fe8a338c012538991
Dhamu-code <dhamodharan130602@gmail.com>
2025-11-20 11:51:05 +0530

    Enrolling course User validation added

--- office_management/router.js
+++ office_management/router.js
@@ -5,9 +5,37 @@ const express = require('express');
 const Router = express.Router();
 
 Router.post('/course', async (req, res) => {
-    const newCourse = new course(req.body);
+   try{
+    const { email } = req.body;
+    
+    const findUser = await user.findOne({ email: email});
+
+    if (!findUser){
+        return res.status(400).json({ error: "User with this email not found"});
+    }
+
+    if (findUser.role !== "instructor"){
+        return res.status(400).json({ error: "User is not an instructor"});
+    }
+
+    const newCourse = new course({
+        ...req.body,
+        userId: findUser._id
+    })
+
     await newCourse.save();
+
     res.status(201).json(newCourse);
+
+   }catch (err){
+    res.status(500).json({ error: err.message});
+   }
+})
+
+Router.post('/user', async (req, res) => {
+    const newUser = new user(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
 })
 
 module.exports = Router;
